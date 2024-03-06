f0e815e979a44142e0e0477d2d4eea5c4e5d0109
Akanshya Bhat <126360297+akanshyabhat@users.noreply.github.com>
2024-03-06 14:53:54 -0800

    Adding coding example documentation for Contact and ContactOption (#25)
    # *Adding coding example documentation for Contact and ContactOption*
    
    ## :recycle: Current situation & Problem
    When I was creating a contact for our application, I was unable to see
    coding examples. These added examples hope to clarify how someone might
    build and implement these features.
    
    **All of these changes were copied over from my previous pull request
    for signed commits.**
    
    
    ## :gear: Release Notes 
    N/A
    
    
    ## :books: Documentation
    - Improve documentation by showing an example of how to build new
    Contact as well as custom ContactOptions embedded within larger body of
    code
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- Sources/SpeziContact/Models/ContactOption.swift
+++ Sources/SpeziContact/Models/ContactOption.swift
@@ -11,6 +11,28 @@ import SwiftUI
 
 
 /// Customizable way to get in contact with an individual and usually connected to a ``Contact``.
+///
+/// ### Usage
+///
+/// The following example demonstrates the usage of the ``ContactOption`` within a ``Contact``. For additional details, refer to the ``Contact`` documentation.
+/// ```swift
+/// Contact(
+///     // other parameters for Contact
+///     contactOptions: [
+///         .call("+1 (123) 456-7890"),
+///         .text("+1 (123) 456-7890"),
+///         .email(addresses: ["email@address.com"]),
+///         // Here is where the custom ContactOption can be used as a part of the contactOptions list
+///         ContactOption(
+///            image: Image(systemName: "safari.fill"),
+///            title: "More Info",
+///            action: {
+///                // Action that should be performed on pressing this ContactOption (i.e. opening a link for a website)...
+///             }
+///         )
+///     ]
+/// )
+/// ```
 public struct ContactOption {
     let id = UUID()
     /// The image representing the ``ContactOption`` in the user interface.
