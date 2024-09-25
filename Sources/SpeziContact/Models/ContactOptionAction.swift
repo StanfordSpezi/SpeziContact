6d556aa254f11de185500b77b898697352786aac
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-09-25 15:11:30 -0700

    reuse

--- Sources/SpeziContact/Models/ContactOptionAction.swift
+++ Sources/SpeziContact/Models/ContactOptionAction.swift
@@ -1,8 +1,9 @@
 //
-//  ContactOptionAction.swift
-//  SpeziContact
+// This source file is part of the Stanford Spezi open-source project
 //
-//  Created by Paul Kraft on 25.09.2024.
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
 //
 
 public protocol ContactOptionAction {
